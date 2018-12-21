3e09e42d47c662ee88329f2dc11b11f869799c32
thanhluan <luan.ttt@neo-lab.vn>
2018-12-21 09:58:47 +0700

    Update css pseudoStyle

--- css-pseudo.js
+++ css-pseudo.js
@@ -1,25 +1,27 @@
 // Use: document.getElementById('test).pseudoStyle('before', 'content', 'This is text for test');
-HTMLElement.prototype.pseudoStyle = function() {
+const pseudoStyle = () => {
   function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
   }
 
-  return function(element,prop,value) {
-      var _this = this;
-      var _sheetId = "pseudoStyles";
-      var _head = document.head || document.getElementsByTagName('head')[0];
-      var _sheet = document.getElementById(_sheetId) || document.createElement('style');
-      _sheet.id = _sheetId;
-      var className = "pseudoStyle" + uuidv4();
+  return function(element, pseudoName, prop, value) {
+    const sheetId = "pseudoStyles";
+    const head = document.head || document.getElementsByTagName('head')[0];
+    const sheet = document.getElementById(sheetId) || document.createElement('style');
+    const className = "pseudoStyle" + uuidv4();
 
-      _this.className +=  " "+className;
+    sheet.id = sheetId;
+    element.className +=  " " + className;
+    sheet.innerHTML += " ." + className + ":" + pseudoName + "{" + prop + ":" + value + "}";
+    head.appendChild(sheet);
 
-      _sheet.innerHTML += " ."+className+":"+element+"{"+prop+":"+value+"}";
-      _head.appendChild(_sheet);
-      return this;
+    return {
+      className,
+      element
+    };
   }
 }();
 
